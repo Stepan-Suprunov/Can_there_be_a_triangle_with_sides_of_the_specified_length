799b914f8e8b97cb3ebe89ad3974003afdfa14a5
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-02 17:41:29 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Может ли существовать треугольник со сторонами указанной длины.
+
+Console.WriteLine("Введите длину стороны А");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длину стороны B");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длину стороны C");
+int C = int.Parse(Console.ReadLine());
+
+if ((A < B+C) && (B < A+C) && (C < A+B)) Console.WriteLine("Такой треугольник может существовать");
+
+else Console.WriteLine("Такой треугольник не может существовать");
